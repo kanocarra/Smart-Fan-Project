1efe2db2218923e85dc318a4b2ff90f217909ea2
kanocarra <emily.melhuish@gmail.com>
2016-08-14 19:30:04 +1200

    Initial app with data syncronisation between real time database working

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Smart-Fan-Project
-Fisher &amp; Paykel Appliances Smart Fan using Firebase Real-time Database and Angular
+# Smart Fan Project
+
+Smart Fan dashboard using Firebase Real-time Database, Angular 1.0 and Chart.js
+
+#To run locally:
+    firebase serve
+    
+#To deploy
+    firebase deploy
+    
+# Hosted at https://smart-fan-project.firebaseapp.com/
+
+
+
---
+++ public/app.js
@@ -0,0 +1,41 @@
+/**
+ * Created by kanocarra on 14/08/16.
+ */
+(function() {
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyBmK3pKvRq8TkgVWZXOgouVRD5YdzWzgzA",
+        authDomain: "smart-fan-project.firebaseapp.com",
+        databaseURL: "https://smart-fan-project.firebaseio.com",
+        storageBucket: "smart-fan-project.appspot.com",
+    };
+    firebase.initializeApp(config);
+
+    angular
+        .module('app', ['firebase'])
+        .controller('Controller', function ($firebaseObject) {
+
+            const database = firebase.database().ref();
+
+            this.object = $firebaseObject(database);
+
+            //Create reference to Smart-Fan status
+            const statusRef = database.child('status');
+            const dataRef = database.child('data');
+
+
+            //Syn status changes (eventType, callback)
+            statusRef.on('value', snap =>
+                console.log(snap.val()));
+
+            //Sync Data Changes
+            dataRef.on('child_added', snap => {
+                console.log(snap.val());
+            });
+        });
+
+
+
+
+}());
